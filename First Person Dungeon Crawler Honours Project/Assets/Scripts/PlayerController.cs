d2836f3439cfded0cbf96687beae5352246a3fd5
Ruadhán O'Muiri <124370802+Ru2102254@users.noreply.github.com>
2025-11-18 00:36:37 +0000

    changed

--- First Person Dungeon Crawler Honours Project/Assets/Scripts/PlayerController.cs
+++ First Person Dungeon Crawler Honours Project/Assets/Scripts/PlayerController.cs
@@ -1,60 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour {
 
     public bool smoothMovement = true;
-    public float movementSpeed = 15f;
+    public bool freeMovement = true;
+    public float movementSpeed = 1f;
     public float movementRotationSpeed = 600f;
+
+    public Transform orientation;
+
     PlayerInput playerInput;
     InputAction moveAction;
+    InputAction rotateAction;
+
+    Vector2 moveDirection;
+    Vector2 lookDirection;
 
     Vector3 toGridPos;
     Vector3 backGridPos;
     Vector3 gridRotation;
 
-
-    void Start()
+    private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions.FindAction("Move");
-        toGridPos = Vector3Int.RoundToInt(transform.position);
+        rotateAction = playerInput.actions.FindAction("Rotate");
     }
 
     void Update()
     {
-        
+        moveDirection = moveAction.ReadValue<Vector2>();
+        lookDirection = rotateAction.ReadValue<Vector2>();
+    }
+    private void FixedUpdate()
+    {
         MovePlayer();
+        RotatePlayer();
     }
 
     void MovePlayer()
     {
-        
-       
-        Vector2 moveDirection = moveAction.ReadValue<Vector2>();
-        //freemovement
-        //transform.position += new Vector3(direction.x, 0, direction.y) * Time.deltaTime;
 
+        //freemovement
+        transform.position += new Vector3(moveDirection.x, 0, moveDirection.y) * movementSpeed * Time.deltaTime;
+        
 
         //Snapping Movement
-        if (true)
+        if (!freeMovement)
         {
             backGridPos = toGridPos;
-            Vector3 targetPosition = toGridPos;
             //Rotation
             if (gridRotation.y > 270 && gridRotation.y < 361) gridRotation.y = 0f;
             if (gridRotation.y < 0f) gridRotation.y = 270f;
             //Movement
             if (!smoothMovement)
             {
-                transform.position = targetPosition;
+                //transform.position = targetPosition;
+                transform.position = new Vector3(moveDirection.x, 0, moveDirection.y) * movementSpeed;
                 transform.rotation = Quaternion.Euler(gridRotation);
             }
             else
             {
-                transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * movementSpeed);
+                transform.position = Vector3.MoveTowards(transform.position, moveDirection, Time.deltaTime * movementSpeed);
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(gridRotation), Time.deltaTime * movementRotationSpeed);
             }
 
@@ -62,6 +74,19 @@ public class PlayerController : MonoBehaviour {
         else
         {
             toGridPos = backGridPos;
+        }
+
+    }
+
+    void RotatePlayer()
+    {
+        if (lookDirection.y != 0)
+        {
+            float roationAmount = lookDirection.x * movementRotationSpeed * Time.deltaTime;
+            Quaternion deltaRotation = Quaternion.Euler(0, roationAmount, 0);
+            transform.rotation *= deltaRotation;
+            //NonFree Rotate
+            //transform.rotation = Quaternion.Euler(0, roationAmount, 0);
         }
 
     }
