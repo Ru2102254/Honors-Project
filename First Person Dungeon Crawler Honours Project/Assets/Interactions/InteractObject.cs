76f0c262ffe943fbd3dc055728324da26cbabe48
Ruadhán O'Muiri <124370802+Ru2102254@users.noreply.github.com>
2026-01-27 11:57:09 +0000

    Placements for all levels complete

--- First Person Dungeon Crawler Honours Project/Assets/Interactions/InteractObject.cs
+++ First Person Dungeon Crawler Honours Project/Assets/Interactions/InteractObject.cs
@@ -23,18 +23,33 @@ public class InteractObject : MonoBehaviour
     {
         onInteract.Invoke();
     }
-    public void MoveFloor()
+    public void MoveFloor(int floorNum)
     {
-        StartCoroutine("MoveFloorCorutine");
+        StartCoroutine("MoveToZeroCorutine");
+        switch (floorNum)
+        {
+            case 0:
+                SceneManager.LoadScene("Level2");
+                break;
+            case 1:
+                SceneManager.LoadScene("Level3");
+                break;
+            case 2:
+                SceneManager.LoadScene("Level4");
+                break;
+            case 3:
+                SceneManager.LoadScene("Level5");
+                break;
+
+        }
     }
-    IEnumerator MoveFloorCorutine()
+    IEnumerator MoveToZeroCorutine()
     {
         DataCarryScript.instance.movementDisabled = true;
         yield return new WaitForSeconds(0.01f);
         player.transform.position = new Vector3(0,1,0);
         yield return new WaitForSeconds(0.01f);
         DataCarryScript.instance.movementDisabled = false;
-        SceneManager.LoadScene("Level2");
         
     }
 
