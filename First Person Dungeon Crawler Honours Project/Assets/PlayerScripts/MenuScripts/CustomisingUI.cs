df791c07c33a420f0c54b890df9251ae1102844a
Ruadhán O'Muiri <124370802+Ru2102254@users.noreply.github.com>
2026-02-23 16:55:39 +0000

    Added start of customisable UI to the battle system

--- First Person Dungeon Crawler Honours Project/Assets/PlayerScripts/MenuScripts/CustomisingUI.cs
+++ First Person Dungeon Crawler Honours Project/Assets/PlayerScripts/MenuScripts/CustomisingUI.cs
@@ -14,25 +14,55 @@ public class CustomisingUI : MonoBehaviour
 
     public GameObject ButtonPanel;
 
+    Vector3 buttonScale = new Vector3(1, 1, 1);
+    Vector3 buttonPosition = new Vector3(1, 1, 1);
 
+    float defaultTextSize = 20;
+    float defaultButton = 1;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void SetTextSize(float textSize)
+    public void SetButtonTextSize(float textSize)
     {
-        FightText.fontSize += textSize;
-        TalkText.fontSize += textSize; 
-        ItemText.fontSize += textSize; 
-        AbscondText.fontSize += textSize;
-        CustomiseText.fontSize += textSize;
-        DialogueText.fontSize += textSize;
-        NameText.fontSize += textSize;
-        LvlText.fontSize += textSize;
+        FightText.fontSize = textSize;
+        TalkText.fontSize = textSize; 
+        ItemText.fontSize = textSize; 
+        AbscondText.fontSize = textSize;
+        CustomiseText.fontSize = textSize;
+        NameText.fontSize = textSize;
+        LvlText.fontSize = textSize;
     }
 
-    public void SetButtonSize(float buttonSize) {
-        
-        Vector3 buttonPosition = Vector3.zero;
-        buttonPosition.x += buttonSize;
-        ButtonPanel.transform.position += buttonPosition;
+    public void SetBattleTextSize(float textSize)
+    {
+        DialogueText.fontSize = textSize;
+    }
+    public void SetButtonWidth(float buttonSize) {
+
+        buttonScale.x = buttonSize;
+        ButtonPanel.transform.localScale = buttonScale;
+    }
+    public void SetButtonHeight(float buttonSize) {
+        buttonScale.y = buttonSize;
+        ButtonPanel.transform.localScale = buttonScale;
+    }
+
+    public void SetButtonX(float buttonPos) {
+        buttonPosition.x = buttonPos;
+        ButtonPanel.transform.position = buttonPosition;
+    }
+    public void SetButtonY(float buttonPos)
+    {
+        buttonPosition.y = buttonPos;
+        ButtonPanel.transform.position = buttonPosition;
+    }
+
+    public void ResetTextSize() {
+        SetButtonTextSize(defaultTextSize);
+    }
+
+    public void ResetButtonSize()
+    {
+        SetButtonWidth(defaultButton);
+        SetButtonHeight(defaultButton);
     }
 }
